9204263b249ee757db17dfbb57f02e0101e41aab
RA. Nur Afifah Widyadhana <widyadhana92@gmail.com>
2024-12-02 21:10:31 +0700

    feat: add search function

--- static/script.js
+++ static/script.js
@@ -18,10 +18,10 @@ const searchBtn = document.getElementById("search");
 const navButton = document.querySelector(".nav-button");
 
 // Ketika tombol search diklik
-searchBtn.addEventListener("click", (e) => {
-  e.preventDefault(); 
-  navButton.classList.toggle("active"); 
-});
+// searchBtn.addEventListener("click", (e) => {
+//   e.preventDefault(); 
+//   navButton.classList.toggle("active"); 
+// });
 
 
 // hamburger menu
@@ -30,3 +30,25 @@ const menu = document.querySelector ('.menu');
 document.querySelector('#hamburger-menu').onclick = () => {
     menu.classList.toggle ('active');
 };
+
+
+// search function
+document.addEventListener("DOMContentLoaded", () => {
+    const searchInput = document.querySelector(".search-bar input");
+    const recipeCards = document.querySelectorAll(".recipe-card");
+    console.log(searchInput);
+
+    searchInput.addEventListener("input", () => {
+        const query = searchInput.value.toLowerCase();
+        console.log(query);
+        recipeCards.forEach(card => {
+            const nama = card.getAttribute("data-name").toLowerCase();
+            const details = card.getAttribute("data-details").toLowerCase();
+            if (nama.includes(query) || details.includes(query)) {
+                card.parentElement.style.display = ""; // Tampilkan
+            } else {
+                card.parentElement.style.display = "none";
+            }
+        });
+    });
+});
